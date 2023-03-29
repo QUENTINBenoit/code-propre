1e0dbde93e6579b615b2b78c9351fb8d1cdef0f7
github_QUENTINBenoit <benquelm@gmail.com>
2023-03-29 15:51:32 +0200

    Exo2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,31 @@
 package ex2;
 
-public class LivretA extends CompteBancaire {
+public class LivretA {
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
-	}
+
+    /**
+     * tauxRemuneration : taux de rémunération dans le cas d'un livret A
+     */
+    private double tauxRemuneration;
+
+    public LivretA(double tauxRemuneration) {
+        this.tauxRemuneration = tauxRemuneration;
+    }
+
+    /** Getter for type
+     * @return the type
+     */
+    public double getTauxRemuneration() {
+        return tauxRemuneration;
+    }
+
+    /**
+     *
+     * @param tauxRemuneration
+     */
+    public void setTauxRemuneration(double tauxRemuneration) {
+
+        this.tauxRemuneration = tauxRemuneration;
+    }
 
 }
