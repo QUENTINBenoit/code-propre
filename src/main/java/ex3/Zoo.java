356457b6a5ebe3823ee2e225399a37fb6e89a710
github_QUENTINBenoit <benquelm@gmail.com>
2023-03-29 17:53:17 +0200

    exo3

--- src/main/java/ex3/Zoo.java
+++ src/main/java/ex3/Zoo.java
@@ -5,29 +5,49 @@ import java.util.List;
 public class Zoo {
 
 	private String nom;
-	private List<String> types;
-	private List<String> noms;
-	private List<String> comportements;
-	
+
+	//private List<String> types;
+	private String types;
+	// private List<String> noms;
+	//private List<String> comportements;
+	private String comportement;
+	private Integer taille;
 	public Zoo(String nom){
 		this.nom = nom;
 	}
-	
+
+	/**
+	 *
+	 * @param nom
+	 * @param types
+	 * @param comportement
+	 * @param taille
+	 */
+	public Zoo(String nom, String types, String comportement, Integer taille) {
+		this.nom = nom;
+		this.types = types;
+		this.comportement = comportement;
+		this.taille = taille;
+	}
+	/*
 	public void addAnimal(String nomAnimal, String typeAnimal, String comportement){
 		types.add(typeAnimal);
 		noms.add(nomAnimal);
 		comportements.add(comportement);
 	}
-	
+	*/
+
+	/*
 	public void afficherListeAnimaux(){
 		for (int i=0; i<types.size(); i++) {
 			System.out.println(noms.get(i)+" "+types.get(i)+" "+comportements.get(i));
 		}
 	}
-	
+
 	public int taille() {
 		return types.size();
 	}
+	*/
 
 	/** Getter for nom
 	 * @return the nom
@@ -41,5 +61,53 @@ public class Zoo {
 	 */
 	public void setNom(String nom) {
 		this.nom = nom;
+	}
+
+	/**
+	 * getteer types
+	 * @return
+	 */
+	public String getTypes() {
+		return types;
+	}
+
+	/**
+	 * Setter tupes
+	 * @param types
+	 */
+	public void setTypes(String types) {
+		this.types = types;
+	}
+
+	/**
+	 * getter comportement
+	 * @return
+	 */
+	public String getComportement() {
+		return comportement;
+	}
+
+	/**
+	 * Setter coportement
+	 * @param comportement
+	 */
+	public void setComportement(String comportement) {
+		this.comportement = comportement;
+	}
+
+	/**
+	 * Getter tailles
+	 * @return
+	 */
+	public Integer getTaille() {
+		return taille;
+	}
+
+	/**
+	 * Setter Taille
+	 * @param taille
+	 */
+	public void setTaille(Integer taille) {
+		this.taille = taille;
 	}
 }
--- src/main/java/ex3/ZooApplication.java
+++ src/main/java/ex3/ZooApplication.java
@@ -1,8 +1,11 @@
 package ex3;
 
+import java.util.ArrayList;
+
 public class ZooApplication {
 
 	public static void main(String[] args) {
+		/*
 		Zoo zoo = new Zoo("Thoiry");
 		
 		zoo.addAnimal("Gazelle", "MAMMIFERE", "HERBIVORE");
@@ -13,6 +16,20 @@ public class ZooApplication {
 		zoo.addAnimal("Truite dorée", "POISSON", "HERBIVORE");
 		zoo.addAnimal("Boa constrictor", "SERPENT", "CARNIVORE");
 		zoo.addAnimal("Python", "SERPENT", "CARNIVORE");
+
+		 */
+       /**
+       * liste des Animaux du Zoo
+        */
+		ArrayList<Zoo> zoo = new ArrayList<>();
+		zoo.add(new Zoo("Gazelle", "MAMMIFERE", "HERBIVORE", 5));
+		zoo.add(new Zoo("Zèbre", "MAMIFERE", "HERBIVORE",  3));
+		zoo.add(new Zoo("Lion", "MAMMIFERE", "HERBIVORE", 2));
+		zoo.add(new Zoo("Panthère", "MAMMIFERE", "CARNIVORE", 1));
+		zoo.add(new Zoo("Requin blanc", "POISSON", "HERBIVORE", 6));
+		zoo.add(new Zoo("Truite dorée", "POISSON", "HERBIVORE", 1));
+		zoo.add(new Zoo("Boa constrictor", "SERPENT", "CARNIVORE",  10));
+		zoo.add(new Zoo("Python", "SERPENT", "CARNIVORE",  13));
 	}
 
 }
