90767f7a358d9025d8c18098b87c5b8562a64eae
github_QUENTINBenoit <benquelm@gmail.com>
2023-03-29 18:18:28 +0200

    Ex04

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,25 @@
+package ex1;
+
+
+public class Calcul {
+
+    /**
+     *  Méthode Addition
+     * @param a
+     * @param b
+     * @return
+     */
+    public int addition(int a, int b){
+        return a+b;
+    }
+
+    /**
+     * Méthode soustraction
+     * @param a
+     * @param b
+     * @return
+     */
+    public int soustraction(int a, int b){
+        return a-b;
+    }
+}
--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -1,19 +1,27 @@
 package ex4;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtils {
-
+/*
 	public static String format(String pattern, Date date) {
-		
+
 		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
 		return formateur.format(date);
 	}
-	
-	public static String formatDefaut(Date date) {
-		
+
+ */
+
+	/**
+	 * farmat date
+	 * @return
+	 */
+	public static String formatDefaut() {
+
 		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		return formateur.format(date);
+		return formateur.format(new Date());
 	}
+
 }
